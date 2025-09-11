ac574bb24c9e5fe1d306b2a04dacec5c0e259a3e
Asim Riaz <asimriazasim107@gmail.com>
2025-09-11 20:02:09 +0500

    ad

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -4,22 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
     <meta name="description" content="Template Content">
     <meta name="keywords" content="Template Keywords">
     <meta name="author" content="Dreamguys - DreamsChat">
 
-    <!-- OpenGraph / Twitter -->
+    <!-- Social / SEO -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@dreamguystech">
-    <meta name="twitter:title" content="Template Content">
-    <meta name="twitter:description" content="Template Content">
-    <meta property="og:url" content="Template Landing Page URL">
     <meta property="og:title" content="Template Title">
     <meta property="og:description" content="Template Content">
-    <meta property="og:image" content="">
-    <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="1200">
-    <meta property="og:image:height" content="600">
 
     <!-- CSRF -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -27,15 +21,14 @@
     <title>{{ config('app.name', 'LogiConn') }}</title>
 
     <!-- Favicon -->
-    <link rel="icon" href="{{ URL::asset('/build/img/AI-Logo.svg') }}">
+    <link rel="icon" href="{{ asset('build/img/AI-Logo.svg') }}">
 
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-    <!-- Vite Assets -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Project CSS (stored in /public/build/css/) -->
+    <link rel="stylesheet" href="{{ asset('build/css/style.css') }}">
 
-    <!-- Extra head includes -->
     @include('layout.partials.head')
 </head>
 <body>
@@ -45,7 +38,9 @@
     </div>
     <!-- /Main Wrapper -->
 
-    <!-- Footer Scripts -->
+    <!-- Project JS (stored in /public/build/js/) -->
+    <script src="{{ asset('build/js/script.js') }}"></script>
+
     @include('layout.partials.footer-scripts')
 </body>
 </html>
