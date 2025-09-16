c2f743972783c40bfa6299c03c7836463ec150e2
Asim Riaz <asimriazasim107@gmail.com>
2025-09-16 10:16:35 +0500

    setting section midddleware add on routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,14 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'), // Hash the password
+            'is_admin' => true,                 // ✅ for boolean flag
+            'type' => 'admin',                  // ✅ for type check
+        ]);
+
+        // (Optional) create some test users
         // User::factory(10)->create();
-
-        \App\Models\User::create([
-        'name' => 'Admin',
-        'email' => 'admin@gmail.com',
-        'password' => Hash::make('123456'), // Hash the password
-    ]);
-
-        
     }
 }
