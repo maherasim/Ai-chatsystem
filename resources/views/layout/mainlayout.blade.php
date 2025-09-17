21fcf47a313e7ace05c38269b982f6e40d6a07b8
Asim Riaz <asimriazasim107@gmail.com>
2025-09-17 17:25:13 +0500

    added

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -26,9 +26,8 @@
         $setting = App\Models\Setting::first();
         $userSetting = auth()->check() ? App\Models\Setting::where('user_id', auth()->id())->first() : null;
     @endphp
-    
-     @dd($setting->favicon);
-     <title>{{ $setting->app_name ?? '' }}</title>
+    <title>{{ $setting->app_name ?? '' }}</title>
+     
     <!-- Favicon -->
     <link rel="icon" href="{{ $setting->favicon ?? asset('/build/img/gallery/gallery-01.jpg') }}" class="rounded-circle">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
