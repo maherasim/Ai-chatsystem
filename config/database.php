2ef89b5898fbb79cae29d030a537b69889e75f70
Asim Riaz <asimriazasim107@gmail.com>
2025-09-29 17:45:11 +0500

    congif change by asim

--- config/database.php
+++ config/database.php
@@ -45,13 +45,13 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('MONGO_DB_HOST', '127.0.0.1'),
-            'port' => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE'),
-            'username' => env('MONGO_DB_USERNAME'),
-            'password' => env('MONGO_DB_PASSWORD'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_CONNECTION'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'options' => [
-                'database' => 'chatsystem'  
+                'database' => 'chatsystem_online'  
             ]
         ],
 /*
