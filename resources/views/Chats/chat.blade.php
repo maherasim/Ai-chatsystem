02e7ab948cfe9ce4927d0d06517f101481cc0ca9
maherasim <asimriazasim107@gmail.com>
2026-01-18 09:31:50 +0500

    chat ui issue fixed

--- resources/views/Chats/chat.blade.php
+++ resources/views/Chats/chat.blade.php
@@ -1004,7 +1004,7 @@
  
 
                 <!-- RIGHT: Settings, Theme Toggle, Logout -->
-                <div class="right-icons d-flex align-items-center gap-1" style="margin-left: auto;">
+                <div class="right-icons d-flex align-items-center" style="margin-left: auto; gap: 0px !important;">
                     <a href="javascript:void(0)" class="btn chat-search-btn" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Search">
                         <img src="{{ asset('/build/img/Search-Black.svg') }}" alt="Search" width="18px">
                         <img src="{{ asset('/build/img/Search-White.svg') }}" alt="Search" width="18px">
